843b6a6a0530536f3e2cbf4e2ab4dfd3fb818398
anetteOlli <ansi@unit.no>
2021-08-30 14:13:19 +0200

    refactoring

--- src/test/java/no/unit/MetadataHandlerTest.java
+++ src/test/java/no/unit/MetadataHandlerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AppTest {
+public class MetadataHandlerTest {
 
     @Test
     public void successfulResponse () {
