04a0aedca15d35bf8fea924fcfd49480c4386289
Gregor Gabriel <gregor.gabriel@unit.no>
2021-09-08 15:05:21 +0200

    test instService kobling

--- src/main/java/no/unit/MetadataHandler.java
+++ src/main/java/no/unit/MetadataHandler.java
@@ -5,11 +5,14 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 
 import java.util.Map;
+import no.unit.ill.services.InstitutionService;
 
 public class MetadataHandler implements RequestHandler<Map<String, Object>, GatewayResponse> {
 
     @Override
     public GatewayResponse handleRequest(Map<String, Object> input, Context context) {
-        return new GatewayResponse("test", 200);
+        InstitutionService institutionService = new InstitutionService();
+        final String libraryCode = institutionService.get("oriaCode", "NTNU_UB", "oriaDefaultNCIPserver");
+        return new GatewayResponse("libaryCode=" + libraryCode, 200);
     }
 }
