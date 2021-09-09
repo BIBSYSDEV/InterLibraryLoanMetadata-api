d88275aa289b9b22b8029f528acd9fe64e040ed8
anetteOlli <ansi@unit.no>
2021-09-09 10:42:29 +0200

    SMILE-929-integration-xservice, tests added

--- src/test/java/no/unit/MetadataHandlerTest.java
+++ src/test/java/no/unit/MetadataHandlerTest.java
@@ -4,11 +4,13 @@ package no.unit;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import no.unit.pnxservice.Pnxervices;
 import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.Response;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 
@@ -22,7 +24,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MetadataHandlerTest {
     public static final String CONDENSED_PNX_EXAMPLE_1 = "condensed_pnx_example_1.json";
-    public static final String DOCUMENT_ID = "BIBSYS_ILS71560264980002201";
 
     private String createJSON(String filename){
 
@@ -45,18 +46,56 @@ public class MetadataHandlerTest {
     }
 
     @Test
-    public void successfulResponse() {
+    public void testingDefaultConstructor(){
+        //for test coverage
+        MetadataHandler app = new MetadataHandler();
+    }
+
+    @Test
+    public void noRecordIdSet(){
         Context awsContext = mock(Context.class);
+        Pnxervices pnxervices = mock(Pnxervices.class);
+        Map<String, Object> event = new HashMap<>();
+        JsonObject condensedExample1 = JsonParser.parseString(createJSON(CONDENSED_PNX_EXAMPLE_1)).getAsJsonObject();
+        MetadataHandler app = new MetadataHandler(pnxervices);
+        when(pnxervices.getPnxData(anyString())).thenReturn(condensedExample1);
+        GatewayResponse result = app.handleRequest(null, awsContext);
+        GatewayResponse result2 = app.handleRequest(event, awsContext);
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), result.getStatusCode());
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), result2.getStatusCode());
+    }
+
+    @Test
+    public void recordIdIsEmptyString(){
+        Context awsContext = mock(Context.class);
+        Pnxervices pnxervices = mock(Pnxervices.class);
         Map<String, Object> event = new HashMap<>();
         Map<String, String> queryParameters = new HashMap<>();
-        String fysiologibok = "BIBSYS_ILS71560264980002201";
+        String leksikon = "";
+        JsonObject condensedExample1 = JsonParser.parseString(createJSON(CONDENSED_PNX_EXAMPLE_1)).getAsJsonObject();
+        queryParameters.put(MetadataHandler.DOCUMENT_ID_KEY, leksikon);
+        event.put(MetadataHandler.QUERY_STRING_PARAMETERS_KEY, queryParameters);
+        MetadataHandler app = new MetadataHandler(pnxervices);
+        when(pnxervices.getPnxData(anyString())).thenReturn(condensedExample1);
+        GatewayResponse result = app.handleRequest(event, awsContext);
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), result.getStatusCode());
+    }
+
+
+
+    @Test
+    public void successfulResponse() {
+        Context awsContext = mock(Context.class);
+        Pnxervices pnxervices = mock(Pnxervices.class);
+        Map<String, Object> event = new HashMap<>();
+        Map<String, String> queryParameters = new HashMap<>();
         String leksikon = "BIBSYS_ILS71463631120002201";
         JsonObject condensedExample1 = JsonParser.parseString(createJSON(CONDENSED_PNX_EXAMPLE_1)).getAsJsonObject();
         queryParameters.put(MetadataHandler.DOCUMENT_ID_KEY, leksikon);
         event.put(MetadataHandler.QUERY_STRING_PARAMETERS_KEY, queryParameters);
-        MetadataHandler app = new MetadataHandler();
+        MetadataHandler app = new MetadataHandler(pnxervices);
+        when(pnxervices.getPnxData(anyString())).thenReturn(condensedExample1);
         GatewayResponse result = app.handleRequest(event, awsContext);
-        System.out.println(result.getBody());
-        assertEquals(200, result.getStatusCode());
+        assertEquals(Response.Status.OK.getStatusCode(), result.getStatusCode());
     }
 }
