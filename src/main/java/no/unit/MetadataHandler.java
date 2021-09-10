148a4bf14b3484c41703ae1b236e03121ad8ddab
anetteOlli <44363838+anetteOlli@users.noreply.github.com>
2021-09-10 10:23:46 +0200

    Smile 929 integration xservice (#19)
    * Refactoring handler into ill package
    
    * xservice parsing
    
    * SMILE-929-integration-xservice, fetched from json rest api
    
    * SMILE-929-integration-xservice, fixed broken tests
    
    * SMILE-929-integration-xservice, added javadoc
    
    * SMILE-929-integration-xservice, fixed imports
    
    * SMILE-929-integration-xservice, pmd errors
    
    * SMILE-929-integration-xservice, refactoring to match inst service
    
    * SMILE-929-integration-xservice, Gregors connection class in use
    
    * SMILE-929-integration-xservice, tests added
    
    * SMILE-929-integration-xservice, tests added
    
    * SMILE-929-integration-xservice, tests added
    
    * SMILE-929-integration-xservice, tests added
    
    * SMILE-929-integration-xservice, removed unused bean
    
    * SMILE-929-integration-xservice, linting
    
    * SMILE-929-integration-xservice, added parameters to template.yaml
    
    * SMILE-929-integration-xservice, passing Primo variables to pipeline
    
    * SMILE-929-integration-xservice, removed auto generated files
    
    * SMILE-929-integration-xservice, removed unused file
    
    * SMILE-929-integration-xservice, updated .gitignore
    
    * SMILE-929-integration-xservice, code-review
    
    * SMILE-929-integration-xservice, adding fnutt
    
    * SMILE-929-integration-xservice, adding fnutts
    
    * SMILE-929-integration-xservice, adding fnutts
    
    * SMILE-929-integration-xservice, code-review spelling

--- src/main/java/no/unit/MetadataHandler.java
+++ src/main/java/no/unit/MetadataHandler.java
@@ -2,17 +2,60 @@ package no.unit;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import no.unit.ill.services.PnxServices;
+
+import javax.ws.rs.core.Response;
+import java.util.Objects;
 
 
 import java.util.Map;
-import no.unit.ill.services.InstitutionService;
 
+import static no.unit.utils.StringUtils.isEmpty;
+
+@SuppressWarnings({"PMD.UnusedPrivateMethod"})
 public class MetadataHandler implements RequestHandler<Map<String, Object>, GatewayResponse> {
+    public static final String QUERY_STRING_PARAMETERS_KEY = "queryStringParameters";
+    public static final String MANDATORY_PARAMETERS_MISSING = "Mandatory parameters 'document_id' is missing.";
+    public static final String INTERNAL_SERVER_ERROR_MESSAGE = "An error occurred, error has been logged";
+    public static final String DOCUMENT_ID_KEY = "document_id";
+    private final transient PnxServices pnxServices;
+
+    public MetadataHandler() {
+        this.pnxServices = new PnxServices();
+    }
+
+    public MetadataHandler(PnxServices pnxServices) {
+        this.pnxServices = pnxServices;
+    }
 
     @Override
     public GatewayResponse handleRequest(Map<String, Object> input, Context context) {
-        InstitutionService institutionService = new InstitutionService();
-        final String libraryCode = institutionService.get("oriaCode", "NTNU_UB", "oriaDefaultNCIPserver");
-        return new GatewayResponse("libaryCode=" + libraryCode, 200);
+        GatewayResponse gatewayResponse = new GatewayResponse();
+
+
+        if (Objects.isNull(input) || !input.containsKey(QUERY_STRING_PARAMETERS_KEY)) {
+            gatewayResponse.setErrorBody(MANDATORY_PARAMETERS_MISSING);
+            gatewayResponse.setStatusCode(Response.Status.BAD_REQUEST.getStatusCode());
+            return gatewayResponse;
+        }
+        @SuppressWarnings("unchecked")
+        Map<String, String> queryStringParameters = (Map<String, String>) input.get(QUERY_STRING_PARAMETERS_KEY);
+        String documentId = queryStringParameters.get(DOCUMENT_ID_KEY);
+
+        if (isEmpty(documentId)) {
+            gatewayResponse.setErrorBody(MANDATORY_PARAMETERS_MISSING);
+            gatewayResponse.setStatusCode(Response.Status.BAD_REQUEST.getStatusCode());
+            return gatewayResponse;
+        }
+        String pnxServiceObject = getXServiceData(documentId);
+        // InstitutionService institutionService = new InstitutionService();
+        // final String libraryCode = institutionService.get("oriaCode", "NTNU_UB", "oriaDefaultNCIPserver");
+        // pnxServiceObject.add("libraryCode", libraryCode);
+        return new GatewayResponse(pnxServiceObject, Response.Status.OK.getStatusCode());
     }
+
+    protected String getXServiceData(String documentId) {
+        return pnxServices.getPnxData(documentId);
+    }
+
 }
