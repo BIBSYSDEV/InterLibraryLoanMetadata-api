c6d4d00fc5f1ed319dc938a27a34f1c1854a48bc
nr <nr@unit.no>
2021-09-14 13:32:44 +0200

    Using nva gatewayresponse

--- src/main/java/no/unit/libcheck/LibcheckResponse.java
+++ src/main/java/no/unit/libcheck/LibcheckResponse.java
@@ -6,25 +6,25 @@ import nva.commons.core.JsonSerializable;
 public class LibcheckResponse implements JsonSerializable {
 
     @JsonProperty("isAlmaLibrary")
-    private boolean isAlmaLibrary;
+    private boolean almaLibrary;
 
     @JsonProperty("isNcipLibrary")
-    private boolean isNcipLibrary;
+    private boolean ncipLibrary;
 
     public boolean isAlmaLibrary() {
-        return isAlmaLibrary;
+        return almaLibrary;
     }
 
     public void setAlmaLibrary(boolean almaLibrary) {
-        isAlmaLibrary = almaLibrary;
+        this.almaLibrary = almaLibrary;
     }
 
     public boolean isNcipLibrary() {
-        return isNcipLibrary;
+        return ncipLibrary;
     }
 
     public void setNcipLibrary(boolean ncipLibrary) {
-        isNcipLibrary = ncipLibrary;
+        this.ncipLibrary = ncipLibrary;
     }
 
 }
