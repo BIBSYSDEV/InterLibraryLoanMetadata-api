9ff04581ac92f67542f0769575ec0007ab5bcef4
Gregor Gabriel <gregor.gabriel@unit.no>
2021-11-25 14:25:42 +0100

    UTF-8 på ncip xml (#161)
    * arm64
    
    * arm64 fungerer ikke bare som det
    
    * SMILE- vedlikeholds mandag
    
    * SMILE- utf-8 på xml payload
    
    * SMILE- utf-8 på xml payload
    
    * SMILE - bumping av verjoner

--- src/main/java/no/unit/ncip/NcipService.java
+++ src/main/java/no/unit/ncip/NcipService.java
@@ -6,8 +6,8 @@ import static org.apache.http.HttpHeaders.CONTENT_TYPE;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -50,13 +50,9 @@ public class NcipService {
 
     protected NcipResponse send(String payload, String ncipServerUrl) {
         HttpPost httppost = new HttpPost(ncipServerUrl);
-        try {
-            httppost.setHeader(CONTENT_TYPE, APPLICATION_XML);
-            httppost.setHeader(ACCEPT, APPLICATION_XML);
-            httppost.setEntity(new StringEntity(payload));
-        } catch (UnsupportedEncodingException e) {
-            log.error(FAILED_TO_SET_PAYLOAD_TO_HTTP_POST, e);
-        }
+        httppost.setHeader(CONTENT_TYPE, APPLICATION_XML);
+        httppost.setHeader(ACCEPT, APPLICATION_XML);
+        httppost.setEntity(new StringEntity(payload, StandardCharsets.UTF_8));
 
         NcipResponse ncipResponse = new NcipResponse();
         try (CloseableHttpResponse response = httpclient.execute(httppost)) {
