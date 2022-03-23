c0072ec5da211348f763a6e0fd2cf5857e8f7dc4
nr <nr@unit.no>
2022-03-23 08:13:32 +0100

    Bugfix charencoding from basebibliotek

--- src/main/java/no/unit/services/BaseBibliotekServiceConnection.java
+++ src/main/java/no/unit/services/BaseBibliotekServiceConnection.java
@@ -11,9 +11,9 @@ import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
 
 import static java.nio.charset.StandardCharsets.ISO_8859_1;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class BaseBibliotekServiceConnection {
 
@@ -53,7 +53,7 @@ public class BaseBibliotekServiceConnection {
             baos.write(buffer, 0, len);
         }
         baos.flush();
-        byte[] bytes = baos.toString(StandardCharsets.UTF_8).getBytes(ISO_8859_1);
+        byte[] bytes = baos.toString(UTF_8).getBytes(ISO_8859_1);
         return new ByteArrayInputStream(bytes);
     }
 }
